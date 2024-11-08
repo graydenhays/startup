d40772d41826a5dae05eafcecbbaea816c68c12f
Grayden Hays <grayden.hays@gmail.com>
2024-11-07 22:16:51 -0700

    Fixing Header components

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Navbar, Nav, Image } from 'react-bootstrap';
 
 function Header() {
     return (
         <header>
             <div className="d-flex flex-row p-2">
-                <img className="me-2" src="user icon.jpg" alt="User Icon" width="30px" height="30px" />
+                <Image className="me-2" src="user icon.jpg" alt="User Icon" width="30px" height="30px" />
                 <p>Hello *profile name here*!!</p>
             </div>
             <h1 className="text-end pe-4">Author Website</h1>
-            <nav>
-                <menu>
-                    <a className="navStyles" href="index.html">Home</a>
-                    <a className="navStyles" href="about.html">About</a>
-                    <a className="navStyles" href="info.html">Info</a>
-                </menu>
-            </nav>
+            <Navbar className='mt-3'>
+                <Nav className='ms-auto'>
+                    <Nav.Link className="navStyles" href="index.html">Home</Nav.Link>
+                    <Nav.Link className="navStyles" href="about.html">About</Nav.Link>
+                    <Nav.Link className="navStyles" href="info.html">Info</Nav.Link>
+                </Nav>
+            </Navbar>
             <hr />
         </header>
     );
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -14,73 +14,71 @@ export function Home(props) {
   const [createURL] = React.useState("index.jsx");
 
   return (
-    <Container>
+    <section>
       <Header />
-      <Row>
-        <Col>
-          <main>
-            <h1 className="p-4">Home Page</h1>
-            {/* Slideshow */}
-            <div>
-              <div>
-                <img className="slideshow" src={image1Url} alt="Merlin's Dragon" />
-                <img className="slideshow" src={image2Url} alt="Code of the Claw" />
-                <img className="slideshow" src={image3Url} alt="Way of Kings" />
-              </div>
-              <p style={{ fontSize: '1.5rem' }} className="d-flex justify-content-center p-4">
-                {bookDesc}
-              </p>
+
+      <main>
+        <h1 className="p-4">Home Page</h1>
+        {/* Slideshow */}
+        <div>
+          <div>
+            <img className="slideshow" src={image1Url} alt="Merlin's Dragon" />
+            <img className="slideshow" src={image2Url} alt="Code of the Claw" />
+            <img className="slideshow" src={image3Url} alt="Way of Kings" />
+          </div>
+          <p style={{ fontSize: '1.5rem' }} className="d-flex justify-content-center p-4">
+            {bookDesc}
+          </p>
+        </div>
+
+        <hr />
+
+        {/* Comments */}
+        <div className="d-flex flex-column p-4">
+          <h3 className="d-flex justify-content-center">Reviews</h3>
+          <Card className="p-2">
+            <Card.Body>Comment #1</Card.Body>
+          </Card>
+          <Card className="p-2">
+            <Card.Body>Comment #2</Card.Body>
+          </Card>
+          <Card className="p-2">
+            <Card.Body>Comment #3</Card.Body>
+          </Card>
+          <div className='d-flex'>
+            <FormControl type="text" placeholder="Add comment here..." />
+            <Button variant='primary' onClick={() => navigate({ commentURL })}>
+              {'Add Comment'}
+            </Button>
+          </div>
+        </div>
+
+        {/* Sign up */}
+        <div className="p-5 d-flex flex-column">
+          <h5>Sign up for monthly updates!</h5>
+          <Form method="get" action="info.html">
+            <Form.Group className="p-2">
+              <Form.Label>@</Form.Label>
+              <FormControl type="text" placeholder="your@email.com" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>🔒</Form.Label>
+              <FormControl type="password" placeholder="password" />
+            </Form.Group>
+
+            <div className="p-2" style={{ display: 'flex', justifyContent: 'end' }}>
+              <Button variant='primary' onClick={() => navigate({ loginURL })}>
+                {'Login'}
+              </Button>
+              <Button variant='primary' onClick={() => navigate({ createURL })}>
+                {'Create'}
+              </Button>
             </div>
+          </Form>
+        </div>
+      </main>
 
-            <hr />
-
-            {/* Comments */}
-            <div className="d-flex flex-column p-4">
-              <h3 className="d-flex justify-content-center">Reviews</h3>
-              <Card className="p-2">
-                <Card.Body>Comment #1</Card.Body>
-              </Card>
-              <Card className="p-2">
-                <Card.Body>Comment #2</Card.Body>
-              </Card>
-              <Card className="p-2">
-                <Card.Body>Comment #3</Card.Body>
-              </Card>
-              <div className='d-flex'>
-                <FormControl type="text" placeholder="Add comment here..." />
-                <Button variant='primary' onClick={() => navigate({ commentURL })}>
-                  {'Add Comment'}
-                </Button>
-              </div>
-            </div>
-
-            {/* Sign up */}
-            <div className="p-5 d-flex flex-column">
-              <h5>Sign up for monthly updates!</h5>
-              <Form method="get" action="info.html">
-                <Form.Group className="p-2">
-                  <Form.Label>@</Form.Label>
-                  <FormControl type="text" placeholder="your@email.com" />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>🔒</Form.Label>
-                  <FormControl type="password" placeholder="password" />
-                </Form.Group>
-
-                <div className="p-2" style={{ display: 'flex', justifyContent: 'end' }}>
-                  <Button variant='primary' onClick={() => navigate({ loginURL })}>
-                    {'Login'}
-                  </Button>
-                  <Button variant='primary' onClick={() => navigate({ createURL })}>
-                    {'Create'}
-                  </Button>
-                </div>
-              </Form>
-            </div>
-          </main>
-        </Col>
-      </Row>
       <Footer />
-    </Container>
+    </section>
   );
 }
